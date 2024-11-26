6d6bec1d53c4e33f507576f69ab0bf06d12de16d
yodhasu <yodhasu@gmail.com>
2024-11-26 15:58:41 +0700

    make easier to run backend and viewer

---
+++ live2dviewer/backendserve.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "cmd /c start-back.bat", false
---
+++ runappandback.bat
@@ -0,0 +1,7 @@
+@echo off
+
+call venv\Scripts\activate
+
+cd live2dviewer
+wscript.exe "backendserve.vbs"
+march7th
